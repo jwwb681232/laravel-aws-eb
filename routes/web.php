24d6f8e57b135e3132f55db6c91f46d59b59cbe6
wangxiao <wangxiao@guadoutech.com>
2022-03-02 16:15:54 +0800

    debug v6

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/nginx-rewrite-test', function () {
+    return view('welcome');
+});
